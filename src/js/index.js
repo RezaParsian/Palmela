c38a101f094cbc159f926e946672d35359257c81
RezaParsian <rezaparsian76@gmail.com>
2024-08-21 23:39:25 +0330

    work on responsive.

--- src/js/index.js
+++ src/js/index.js
@@ -2,46 +2,70 @@ import Swiper from 'swiper';
 import {Autoplay, Navigation, Pagination} from 'swiper/modules';
 import '../../node_modules/swiper/swiper.min.css';
 
-Swiper.use([Autoplay,Pagination,Navigation]);
+Swiper.use([Autoplay, Pagination, Navigation]);
 
 new Swiper('#swiper_brands', {
 	loop: true,
-	slidesPerView: 6,
 	spaceBetween: 30,
 	autoplay: {
 		delay: 2500,
-		pauseOnMouseEnter:true
+		pauseOnMouseEnter: true
 	},
+	breakpoints: {
+		300: {
+			slidesPerView: 2,
+		},
+		800:{
+			slidesPerView: 6,
+
+		}
+	}
 });
 
 
 new Swiper('#swiper_solutions', {
 	loop: true,
-	slidesPerView: 3.4,
-	spaceBetween:40,
+	spaceBetween: 40,
 	autoplay: {
 		delay: 2500,
-		pauseOnMouseEnter:true
+		pauseOnMouseEnter: true
 	},
+	breakpoints: {
+		300: {
+			slidesPerView: 1,
+		},
+		800:{
+			slidesPerView: 3.4,
+
+		}
+	}
 });
 
 new Swiper('#swiper_testimonial', {
 	loop: true,
 	slidesPerView: 1.8,
-	spaceBetween:40,
+	spaceBetween: 40,
 	autoplay: {
 		delay: 4000,
-		pauseOnMouseEnter:true
+		pauseOnMouseEnter: true
 	},
 	pagination: {
 		el: "#swiper_testimonial_pagination",
 		clickable: true,
 		renderBullet: function (index, className) {
-				return '<span class="' + className + '">' + (String(index + 1).padStart(2,0)) + "</span>";
+			return '<span class="' + className + '">' + (String(index + 1).padStart(2, 0)) + "</span>";
 		},
 	},
 	navigation: {
 		nextEl: "#swiper_testimonial_swiper_button_next",
 		prevEl: "#swiper_testimonial_swiper_button_prev",
 	},
+});
+
+document.getElementById('offCanvasMenuBtn').addEventListener('click', function () {
+	document.getElementById('offCanvas').style.width = '100vw';
+});
+
+document.getElementById('offCanvasCloseMenuBtn').addEventListener('click', function () {
+	document.getElementById('offCanvas').style.width = '0vw'
 });
